9641bc261c727405cd20b32a3ab58ee797512a2b
mitsu <mitsush.g@gmail.com>
2024-02-05 05:59:55 +0600

    update on Lab3

---
+++ lab3/Functions1/all_in_one.py
@@ -0,0 +1,27 @@
+import first_task
+import second_task
+import third_task
+import fourth_task
+import fifth_task
+import sixth_task
+import seventh_task
+import eighth_task
+import ninth_task
+import tenth_task
+import eleventh_task
+import twelth_task
+import thirteenth_task
+
+result1 = first_task.grams_to_ounces()
+result2 = second_task.fahrenheit_to_centigrade()
+result3 = third_task.solution()
+result4 = fourth_task.filter_prime()
+result5 = fifth_task.permute()
+result6 = sixth_task.reverse_str()
+result7 = seventh_task.three_three()
+result8 = eighth_task.o_o_seven()
+result9 = ninth_task.volume_of_sphere()
+result10 = tenth_task.unique_elements()
+result11 = eleventh_task.palindrome()
+result12 = twelth_task.histogram()
+result13 = thirteenth_task.guess_the_num()
